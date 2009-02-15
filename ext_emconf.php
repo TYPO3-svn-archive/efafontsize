3ae5bf608df3d0fa8b1c6292b2a95dfa01837f92
francois <francois@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-02-15 21:15:44 +0000

    Corrected cookie handling to handle only fontSize cookie Simplified cookie domain definition Updated manual to new template Added note in manual about not using this script Released to TER as version 1.3.0
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/efafontsize/trunk@16887 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "efafontsize"
 #
-# Auto generated 06-04-2008 22:21
+# Auto generated 15-02-2009 22:13
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'JavaScript font resizing tool based on the script from Einfach fuer Alle (http://www.einfach-fuer-alle.de/artikel/fontsize/)',
 	'category' => 'plugin',
 	'author' => 'Francois Suter (Cobweb)',
-	'author_email' => 'support@cobweb.ch',
+	'author_email' => 'typo3@cobweb.ch',
 	'shy' => '',
 	'dependencies' => '',
 	'conflicts' => '',
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '1.2.2',
+	'version' => '1.3.0',
 	'constraints' => array(
 		'depends' => array(
 		),
@@ -38,7 +38,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:15:{s:9:"ChangeLog";s:4:"f396";s:10:"README.txt";s:4:"0d77";s:12:"ext_icon.gif";s:4:"fa9d";s:17:"ext_localconf.php";s:4:"654a";s:14:"ext_tables.php";s:4:"dd72";s:14:"doc/manual.sxw";s:4:"467f";s:32:"pi1/class.tx_efafontsize_pi1.php";s:4:"9d69";s:17:"pi1/locallang.xml";s:4:"d7bf";s:20:"res/icons/bigger.gif";s:4:"22a1";s:19:"res/icons/reset.gif";s:4:"3ec7";s:21:"res/icons/smaller.gif";s:4:"736e";s:17:"res/js/cookies.js";s:4:"5ed2";s:22:"res/js/efa_fontsize.js";s:4:"69ab";s:20:"static/constants.txt";s:4:"7a02";s:16:"static/setup.txt";s:4:"d73b";}',
+	'_md5_values_when_last_written' => 'a:15:{s:9:"ChangeLog";s:4:"4549";s:10:"README.txt";s:4:"0d77";s:12:"ext_icon.gif";s:4:"fa9d";s:17:"ext_localconf.php";s:4:"654a";s:14:"ext_tables.php";s:4:"dd72";s:14:"doc/manual.sxw";s:4:"736f";s:32:"pi1/class.tx_efafontsize_pi1.php";s:4:"9652";s:17:"pi1/locallang.xml";s:4:"d7bf";s:20:"res/icons/bigger.gif";s:4:"22a1";s:19:"res/icons/reset.gif";s:4:"3ec7";s:21:"res/icons/smaller.gif";s:4:"736e";s:17:"res/js/cookies.js";s:4:"71e4";s:22:"res/js/efa_fontsize.js";s:4:"69ab";s:20:"static/constants.txt";s:4:"7a02";s:16:"static/setup.txt";s:4:"d73b";}',
 	'suggests' => array(
 	),
 );
--- pi1/class.tx_efafontsize_pi1.php
+++ pi1/class.tx_efafontsize_pi1.php
@@ -2,7 +2,7 @@
 /***************************************************************
 *  Copyright notice
 *
-*  (c) 2007 Francois Suter (Cobweb) <support@cobweb.ch>
+*  (c) 2007-2009 Francois Suter (Cobweb) <typo3@cobweb.ch>
 *  All rights reserved
 *
 *  This script is part of the TYPO3 project. The TYPO3 project is
@@ -30,7 +30,7 @@ require_once(PATH_tslib.'class.tslib_pibase.php');
 /**
  * Plugin 'Dynamic Font Resizer' for the 'efafontsize' extension.
  *
- * @author	Francois Suter (Cobweb) <support@cobweb.ch>
+ * @author	Francois Suter (Cobweb) <typo3@cobweb.ch>
  * @package	TYPO3
  * @subpackage	tx_efafontsize
  */
