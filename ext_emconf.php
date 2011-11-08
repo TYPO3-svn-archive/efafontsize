6f0fe5bd5bdc6b52daf303d6d60a12d84d38d5e5
francois <francois@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-11-08 15:59:57 +0000

    Released to TER as version 1.3.1
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/efafontsize/trunk@53905 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,13 +1,13 @@
 <?php
 
 ########################################################################
-# Extension Manager/Repository config file for ext: "efafontsize"
+# Extension Manager/Repository config file for ext "efafontsize".
 #
-# Auto generated 15-02-2009 22:13
+# Auto generated 08-11-2011 16:10
 #
 # Manual updates:
-# Only the data in the array - anything else is removed by next write.
-# "version" and "dependencies" must not be touched!
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
 ########################################################################
 
 $EM_CONF[$_EXTKEY] = array(
@@ -29,16 +29,17 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '1.3.0',
+	'version' => '1.3.1',
 	'constraints' => array(
 		'depends' => array(
+			'typo3' => '3.5.0-4.6.99',
 		),
 		'conflicts' => array(
 		),
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:15:{s:9:"ChangeLog";s:4:"4549";s:10:"README.txt";s:4:"0d77";s:12:"ext_icon.gif";s:4:"fa9d";s:17:"ext_localconf.php";s:4:"654a";s:14:"ext_tables.php";s:4:"dd72";s:14:"doc/manual.sxw";s:4:"736f";s:32:"pi1/class.tx_efafontsize_pi1.php";s:4:"9652";s:17:"pi1/locallang.xml";s:4:"d7bf";s:20:"res/icons/bigger.gif";s:4:"22a1";s:19:"res/icons/reset.gif";s:4:"3ec7";s:21:"res/icons/smaller.gif";s:4:"736e";s:17:"res/js/cookies.js";s:4:"71e4";s:22:"res/js/efa_fontsize.js";s:4:"69ab";s:20:"static/constants.txt";s:4:"7a02";s:16:"static/setup.txt";s:4:"d73b";}',
+	'_md5_values_when_last_written' => 'a:16:{s:9:"ChangeLog";s:4:"1739";s:10:"README.txt";s:4:"0d77";s:12:"ext_icon.gif";s:4:"fa9d";s:17:"ext_localconf.php";s:4:"654a";s:14:"ext_tables.php";s:4:"dd72";s:14:"doc/manual.pdf";s:4:"b6a0";s:14:"doc/manual.sxw";s:4:"7e14";s:32:"pi1/class.tx_efafontsize_pi1.php";s:4:"12fe";s:17:"pi1/locallang.xml";s:4:"d7bf";s:20:"res/icons/bigger.gif";s:4:"22a1";s:19:"res/icons/reset.gif";s:4:"3ec7";s:21:"res/icons/smaller.gif";s:4:"736e";s:17:"res/js/cookies.js";s:4:"71e4";s:22:"res/js/efa_fontsize.js";s:4:"69ab";s:20:"static/constants.txt";s:4:"7a02";s:16:"static/setup.txt";s:4:"d73b";}',
 	'suggests' => array(
 	),
 );
